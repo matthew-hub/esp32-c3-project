055076f81d6217ebdc55770a0a13e61f9b16e4be
matthew-hub <mateuszr430@gmail.com>
2023-02-26 18:21:14 +0100

    small change, add functionality of battery % read, handle adc readings

--- main/main.c
+++ main/main.c
@@ -1,20 +1,26 @@
 // ESP-IDF:
 #include <stdio.h>
+#include <stdlib.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <freertos/queue.h>
 #include <driver/gptimer.h>
 #include <driver/gpio.h>
+#include <soc/soc_caps.h>
+#include <esp_adc/adc_oneshot.h>
+#include <esp_adc/adc_cali.h>
+#include <esp_adc/adc_cali_scheme.h>
 #include <esp_err.h>
 #include <esp_log.h>
 #include <esp_system.h>
+#include <string.h>
 // COMPONENT:
 #include <button.h>
 #include <aht.h>
 // HEADER:
 #include "num_pattern.h"
 
-static const char *TAG = "74HC595N";
+static const char *TAG = "ESP32-C3";
 
 // 74HC595N PIN DESCRIPTION | SHIFT REGISTER
 #define SER_PIN GPIO_NUM_20   // SERIAL DATA INPUT PIN
@@ -41,9 +47,20 @@ static const char *TAG = "74HC595N";
 // array of digits pin
 const uint8_t DIG_PINS[4] = {DIG_1_PIN, DIG_2_PIN, DIG_3_PIN, DIG_4_PIN};
 // array of numbers to display
-uint8_t NUMBERS_TO_DISPLAY[4] = {2, 4, 8, 3};
+uint8_t NUMBERS_TO_DISPLAY[4] = {2, 0, 0, 1};
 // order of the digit
 int ORDER_DIGITS = 0;
+
+/*---------------------------------------------------------------
+        ADC General Macros
+---------------------------------------------------------------*/
+// ADC1 Channels
+#define EXAMPLE_ADC1_CHAN0 ADC_CHANNEL_0
+
+static int adc_raw[2][10];
+static int voltage[2][10];
+static bool example_adc_calibration_init(adc_unit_t unit, adc_atten_t atten, adc_cali_handle_t *out_handle);
+static void example_adc_calibration_deinit(adc_cali_handle_t handle);
 
 static const char *states[] = {
     [BUTTON_PRESSED] = "pressed",
@@ -188,10 +205,12 @@ void app_main(void)
   gpio_set_level(DIG_3_PIN, 1);
   gpio_set_level(DIG_4_PIN, 1);
 
+  // gpio_set_direction(GPIO_NUM_10, GPIO_MODE_OUTPUT);
+  // gpio_set_level(GPIO_NUM_10, 1);
   // First button connected between GPIO and GND
   // pressed logic level 0, no autorepeat
-  btn1.gpio = GPIO_NUM_7;
-  btn1.pressed_level = 0;
+  btn1.gpio = GPIO_NUM_10;
+  btn1.pressed_level = 1;
   btn1.internal_pull = true;
   btn1.autorepeat = false;
   btn1.callback = on_button;
@@ -232,5 +251,122 @@ void app_main(void)
   ESP_ERROR_CHECK(gptimer_start(gptimer));
 
   ESP_ERROR_CHECK(i2cdev_init());
-  xTaskCreate(aht20_task, "AHT_20:", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL);
+  // xTaskCreate(aht20_task, "AHT_20:", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL);
+
+  //-------------ADC1 Init---------------//
+  adc_oneshot_unit_handle_t adc1_handle;
+  adc_oneshot_unit_init_cfg_t init_config1 = {
+      .unit_id = ADC_UNIT_1,
+  };
+  ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
+
+  //-------------ADC1 Config---------------//
+  adc_oneshot_chan_cfg_t config = {
+      .bitwidth = ADC_BITWIDTH_12,
+      .atten = ADC_ATTEN_DB_11,
+  };
+  ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, EXAMPLE_ADC1_CHAN0, &config));
+
+  //-------------ADC1 Calibration Init---------------//
+  adc_cali_handle_t adc1_cali_handle = NULL;
+  bool do_calibration1 = example_adc_calibration_init(ADC_UNIT_1, ADC_ATTEN_DB_11, &adc1_cali_handle);
+  float battery = 0.0;
+  const float battery_max = 4.20; // maximum voltage of battery
+  const float battery_min = 3.0;  // minimum voltage of battery before shutdown
+  float b_v = 0.0;
+  while (1)
+  {
+    ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN0, &adc_raw[0][0]));
+    ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, adc_raw[0][0]);
+    if (do_calibration1)
+    {
+      ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_handle, adc_raw[0][0], &voltage[0][0]));
+      b_v = voltage[0][0] * 2;
+      battery = ((((b_v / 1000) - battery_min) / (battery_max - battery_min)) * 100);
+      printf("B: %.3f \n", battery);
+      // printf("B: %.3f \n", (((b_v/1000) - battery_min) / (battery_max - battery_min)) * 100);
+      ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, voltage[0][0]);
+      ESP_LOGI(TAG, "ADC%d Channel[%d] Battery Percentage: %.2f ", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, battery);
+    }
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+
+  // Tear Down
+  ESP_ERROR_CHECK(adc_oneshot_del_unit(adc1_handle));
+  if (do_calibration1)
+  {
+    example_adc_calibration_deinit(adc1_cali_handle);
+  }
+}
+
+/*---------------------------------------------------------------
+        ADC Calibration
+---------------------------------------------------------------*/
+static bool example_adc_calibration_init(adc_unit_t unit, adc_atten_t atten, adc_cali_handle_t *out_handle)
+{
+  adc_cali_handle_t handle = NULL;
+  esp_err_t ret = ESP_FAIL;
+  bool calibrated = false;
+
+#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
+  if (!calibrated)
+  {
+    ESP_LOGI(TAG, "calibration scheme version is %s", "Curve Fitting");
+    adc_cali_curve_fitting_config_t cali_config = {
+        .unit_id = unit,
+        .atten = atten,
+        .bitwidth = ADC_BITWIDTH_12,
+    };
+    ret = adc_cali_create_scheme_curve_fitting(&cali_config, &handle);
+    if (ret == ESP_OK)
+    {
+      calibrated = true;
+    }
+  }
+#endif
+
+#if ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
+  if (!calibrated)
+  {
+    ESP_LOGI(TAG, "calibration scheme version is %s", "Line Fitting");
+    adc_cali_line_fitting_config_t cali_config = {
+        .unit_id = unit,
+        .atten = atten,
+        .bitwidth = ADC_BITWIDTH_DEFAULT,
+    };
+    ret = adc_cali_create_scheme_line_fitting(&cali_config, &handle);
+    if (ret == ESP_OK)
+    {
+      calibrated = true;
+    }
+  }
+#endif
+
+  *out_handle = handle;
+  if (ret == ESP_OK)
+  {
+    ESP_LOGI(TAG, "Calibration Success");
+  }
+  else if (ret == ESP_ERR_NOT_SUPPORTED || !calibrated)
+  {
+    ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
+  }
+  else
+  {
+    ESP_LOGE(TAG, "Invalid arg or no memory");
+  }
+
+  return calibrated;
+}
+
+static void example_adc_calibration_deinit(adc_cali_handle_t handle)
+{
+#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
+  ESP_LOGI(TAG, "deregister %s calibration scheme", "Curve Fitting");
+  ESP_ERROR_CHECK(adc_cali_delete_scheme_curve_fitting(handle));
+
+#elif ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
+  ESP_LOGI(TAG, "deregister %s calibration scheme", "Line Fitting");
+  ESP_ERROR_CHECK(adc_cali_delete_scheme_line_fitting(handle));
+#endif
 }
